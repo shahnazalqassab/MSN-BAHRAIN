18b73500b7e4164ef7adfee0bc4f0ff6a71c71bc
nmq2111 <nooraq123@gmail.com>
2025-05-14 16:30:47 +0300

    Packages

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express')
+const bcrypt = require('bcrypt')
+const dotenv = require('dotenv').config()
+const ejs = require('ejs')
+const expressSession = require('express-session')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const path = require('path')
+
+mongoose.connect(process.env.MONGODB_URI)
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`)
+})
+
 const app = express()
+const PORT = process.env.PORT
 
 app.get('/', (req, res) => {
   res.send('HELLO')
 })
-const PORT = 2000
+
 app.listen(PORT, () => {
   console.log(`Hello from ${PORT} Post`)
 })
