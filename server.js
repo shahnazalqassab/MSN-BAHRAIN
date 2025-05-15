02f672ff7369b8f5fc6296068092b5b262328f11
Mohdsaad83 <mfm12208@gmail.com>
2025-05-15 09:52:58 +0300

    signin

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sign Up</title>
-    <link rel="stylesheet" href="/css/style.css">
-    </head>
+const express = require('express')
+const bcrypt = require('bcrypt')
+const dotenv = require('dotenv').config()
+const ejs = require('ejs')
+const expressSession = require('express-session')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const path = require('path')
+mongoose.connect(process.env.MONGODB_URI)
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`)
+})
+const app = express()
+const PORT = process.env.PORT
+app.get('/', (req, res) => {
+  res.send('HELLO')
+})
+app.listen(PORT, () => {
+  console.log(`Hello from ${PORT} Post`)
+})
