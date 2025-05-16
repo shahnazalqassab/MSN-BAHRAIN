26939d2ba25c120ec00da0c3f7a61c6496f5cb9c
nmq2111 <nooraq123@gmail.com>
2025-05-16 15:58:42 +0300

    craete

--- controllers/Ads.js
+++ controllers/Ads.js
@@ -1,8 +1,25 @@
-const express = require('express');
-const User = require('../models/users.js');
+const express = require('express')
+const User = require('../models/users.js')
 
-const router = express.Router();
+const router = express.Router()
 
+//:get:index
 router.get('/', async (req, res) => {
-  res.render('')
-});
+  const currentUser = await User.findById(req.session.user._id)
+  res.render('Ads/index.ejs', { Ads: currentUser.Ads })
+})
+
+//:get:new
+router.get('/new', async (req, res) => {
+  res.render('Ads/new.ejs')
+})
+
+//:get:Create
+router.post('/', async (req, res) => {
+  const currentUser = await User.findById(req.session.user._id)
+  currentUser.Ads.push(req.body)
+  await currentUser.save()
+  req.redirect(`/users/${currentUser._id}/Ads`)
+})
+
+module.exports = router
--- controllers/user.js
+++ controllers/user.js
@@ -1,64 +1,68 @@
-const express = require('express');
-const router = express.Router();
-const bcrypt = require("bcrypt");
-const User = require('../models/users');
-
-
+const express = require('express')
+const router = express.Router()
+const bcrypt = require('bcrypt')
+const User = require('../models/users')
 
 //signin
-router.get("/signIn", (req, res) => {
-  res.render("Auth/signIn.ejs");
-});
+router.get('/signIn', (req, res) => {
+  res.render('Auth/signIn.ejs')
+})
 
-router.post("/signIn", async (req, res) => {
+router.post('/signIn', async (req, res) => {
   try {
-    const user = await User.findOne({ username: req.body.username });
+    const user = await User.findOne({ username: req.body.username })
     if (!user || !bcrypt.compareSync(req.body.password, user.password)) {
-      return res.send("Invalid credentials");
+      return res.send('Invalid credentials')
     }
 
-    req.session.user = { _id: user._id, username: user.username };
-    res.redirect("/");
+    req.session.user = { _id: user._id, username: user.username }
+    res.redirect('/')
   } catch (error) {
-    console.error(error);
-    res.redirect("/Auth/signIn");
+    console.error(error)
+    res.redirect('/Auth/signIn')
   }
-});
+})
 
 //signUp router
+router.get('/signUp', (req, res) => {
+  res.render('Auth/signUp.ejs')
+})
 
-router.get("/signUp", (req, res) => {
-    res.render("Auth/signUp.ejs");
-});
+router.post('/signUp', async (req, res) => {
+  const userExists = await User.findOne({ username: req.body.username })
+  if (userExists) {
+    return res.send('Username already taken')
+  }
+  if (req.body.password !== req.body.confirmPassword) {
+    return res.send('Passwords do not match')
+  }
 
-router.post("/signUp", async (req, res) => {
-    const userExists = await User.findOne({ username: req.body.username });
-    if (userExists) {
-        return res.send("Username already taken");
-    }
-    if (req.body.password !== req.body.confirmPassword) {
-        return res.send("Passwords do not match");
-    }
+  const hashedPassword = bcrypt.hashSync(req.body.password, 10)
+  req.body.password = hashedPassword
 
-    const hashedPassword = bcrypt.hashSync(req.body.password, 10);
-    req.body.password = hashedPassword;
+  const user = await User.create(req.body)
 
+  req.session.user = {
+    username: user.username,
+    _id: user._id
+  }
 
-    const user = await User.create(req.body);
+  req.session.save(() => {
+    res.redirect('/')
+  })
+})
 
-    req.session.user = {
-        username: user.username,
-        _id: user._id
-    };
+router.get('/signUp', (req, res) => {
+  res.render('Auth/signUp.ejs')
+})
 
-    req.session.save(() => {
-        res.redirect("/");
-    });
-});
+router.get('/signOut', (req, res) => {
+  req.session.destroy()
+  res.redirect('/')
+})
 
-router.get("/signUp", (req, res) => {
-  res.render("Auth/signUp.ejs");
-});
+router.get('/index', (req, res) => {
+  res.render('Auth/index.ejs')
+})
 
-module.exports = router;
-
+module.exports = router
