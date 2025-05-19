fb06048b363bc3112ad04bfa486af1002ea4eb76
nmq2111 <nooraq123@gmail.com>
2025-05-19 11:11:00 +0300

    Show

--- controllers/Ads.js
+++ controllers/Ads.js
@@ -4,39 +4,40 @@ const User = require('../models/users.js')
 const router = express.Router()
 
 //:get:index
-router.get('/', async (req, res) => {
-  const currentUser = await User.findById(req.session.user._id) // LOOKING UP THE CURRENT USER
-  // console.log(currentUser.applications);
-  res.render('Ads/index.ejs', {
-    Ads: currentUser.user._id
-  }) // RENDERING THE PAGE WITH HIS DETAILS
-})
+// router.get('/', async (req, res) => {
+//   const currentUser = await User.findById(req.session.user._id) // LOOKING UP THE CURRENT USER
+//   // console.log(currentUser.applications);
+//   res.render('Ads/index.ejs', {
+//     Ads: currentUser.user._id
+//   }) // RENDERING THE PAGE WITH HIS DETAILS
+// })
 
 //:get:new
 router.get('/:userId/new', async (req, res) => {
   const currentUser = await User.findById(req.params.userId)
-  console.log(currentUser)
   res.render('Ads/new.ejs', { user: currentUser })
 })
 
 //:get:Create
 router.post('/:userId/Ads', async (req, res) => {
-  try {
-    const currentUser = await User.findById(req.session.user._id)
-    currentUser.Ads.push(req.body)
-    await currentUser.save()
-    res.redirect(`/Ads/${currentUser._id}/Ads`)
-  } catch (err) {
-    console.log(err)
-    res.redirect(`/`)
-  }
+  const currentUser = await User.findById(req.session.user._id)
+  currentUser.Ads.push(req.body)
+  await currentUser.save()
+  res.redirect(`/Ads/${currentUser._id}/Ads`)
 })
 
+//get:index
 router.get('/:userId/Ads', async (req, res) => {
   const currentUser = await User.findById(req.session.user._id)
   const Ads = currentUser.Ads
-  console.log(Ads)
   res.render('Ads/index.ejs', { Ads })
+})
+
+//get:show
+router.get('/:userId/Ads/:AdsId', async (req, res) => {
+  const currentUser = await User.findById(req.session.user._id)
+  const Ads = currentUser.Ads.id(req.params.AdsId)
+  res.render('Ads/show.ejs', { ad: Ads })
 })
 
 module.exports = router
