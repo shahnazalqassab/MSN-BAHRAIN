8e7bb2cf30b2ac770daad7d611857d35fae309c7
Mohdsaad83 <mfm12208@gmail.com>
2025-05-21 11:56:40 +0300

    Hello

--- models/users.js
+++ models/users.js
@@ -8,7 +8,7 @@ const AdsSchema = new mongoose.Schema(
     title: { type: String, required: true },
     price: { type: Number, required: true },
     description: { type: String, required: true },
-    category: { type: String, enum: ['books', 'phones', 'cars', 'spare parts'] }
+    category: { type: String, enum: ['books', 'phones', 'cars', 'spare parts', 'laptop'] }
   },
   { timestamps: true }
 )
